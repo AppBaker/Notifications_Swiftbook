eb52932b75d7f6fcafb7b463bb6ce0ae5756f8c4
Ivan Nikitin <nqqaqq@gmail.com>
2019-08-01 02:09:57 +0700

    Refactoring create Notifications class

--- Notifications/AppDelegate.swift
+++ Notifications/AppDelegate.swift
@@ -11,108 +11,26 @@ import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var bage = 0
+    
     var window: UIWindow?
-    let notificationCenter = UNUserNotificationCenter.current()
+    let notifications = Notifications()
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        requestAutorization()
-        notificationCenter.delegate = self
+        notifications.requestAutorization()
+        notifications.notificationCenter.delegate = notifications
         return true
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        bage = 0
+        notifications.bage = 0
         
        UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
-    func requestAutorization() {
-        notificationCenter.requestAuthorization(options: [.alert,.badge,.sound]) { (granted, error) in
-            print("Permition granted :", granted)
-            
-            guard granted else { return }
-            self.getNotificationSettings()
-            
-        }
-    }
-    
-    func getNotificationSettings() {
-        notificationCenter.getNotificationSettings { (settings) in
-            print("Nottification settings \(settings)")
-        }
-    }
-    
-    func scheduelNotification(notificationType: String) {
-        bage += 1
-        let content = UNMutableNotificationContent()
-        let userAction = "User Action"
-        
-        content.title = notificationType
-        content.body = "This is example how to create \(notificationType)"
-        content.sound = .default
-        content.badge = bage as NSNumber
-        content.categoryIdentifier = userAction
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        
-        let identifire = notificationType
-        
-        let request = UNNotificationRequest(identifier: identifire, content: content, trigger: trigger)
-        
-        notificationCenter.add(request) { (error) in
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-            }
-        }
-        
-        let snoozeAction = UNNotificationAction(identifier: "Snooze", title: "Snooze", options: [])
-        let deleteAction = UNNotificationAction(identifier: "Delete", title: "Delete", options: [.destructive])
-        
-        let category = UNNotificationCategory(identifier: userAction,
-                                              actions: [snoozeAction, deleteAction],
-                                              intentIdentifiers: [],
-                                              options: [])
-        notificationCenter.setNotificationCategories([category])
-    }
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        
-        completionHandler([.alert, .sound])
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        
-//            let alertController = UIAlertController(title: "Notification", message: response.notification.request.identifier, preferredStyle: .alert)
-//            let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
-//            alertController.addAction(okAction)
-        
-        
-        print ("Handling notification with the \(response.notification.request.identifier)")
-        
-        switch response.actionIdentifier {
-        case UNNotificationDismissActionIdentifier:
-            print("Dismiss Action")
-        case UNNotificationDefaultActionIdentifier:
-            print("Default")
-        case "Snooze":
-            print("Snooze")
-            scheduelNotification(notificationType: "Reminder")
-        case "Delete":
-            print("Delete")
-        default:
-            print("Unknown action")
-        }
-        
-        completionHandler()
     }
 
 }
